70ba5911fe2b3cc95ddb6031a4dabb5783be02f1
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-11-25 20:44:02 +0300

    the main script

--- youtube-set.sh
+++ youtube-set.sh
@@ -21,6 +21,7 @@ row=$(zenity --forms --title="YouTube API Settings" --text="Input data" \
 akey=$(echo $row | cut -f1 -d "|"); cid=$(echo $row | cut -f2 -d "|")
 csec=$(echo $row | cut -f3 -d "|")
 
+if [[ "$akey" && "$cid" && "$csec" ]]; then
 sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'$akey'<\/setting>/g' "$fname"
 sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'$cid'<\/setting>/g' "$fname"
 sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'$csec'<\/setting>/g' "$fname"
@@ -28,5 +29,8 @@ sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'$
 zenity --info --no-wrap --text \
 "The API settings of the plugin.video.youtube were saved in the \
 file:\n/home/$USER/.kodi/userdata/addon_data/plugin.video.youtube/settings.xml"
+    else
+zenity --warning --no-wrap --text "Empty values are not allowed!"
+fi;
 
 exit;
