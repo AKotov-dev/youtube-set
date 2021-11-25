1f296caff12a6c7e7c4e56eb1586f0d611654b0e
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-11-25 19:08:21 +0300

    the main script

--- youtube-set.sh
+++ youtube-set.sh
@@ -2,15 +2,17 @@
 
 fname="/home/$USER/.kodi/userdata/addon_data/plugin.video.youtube/settings.xml"
 
-row=$(zenity --forms --separator=" " --title="YouTube API Settings" --text="Input data" \
+row=$(zenity --forms --title="YouTube API Settings" --text="Input data" \
    --add-entry="API_Key" \
    --add-entry="Client_ID" \
    --add-entry="Client_Secret")
 
 [[ "$?" != "0" ]] && exit 1
 
-akey=$(echo $row | awk '{ print $1 }'); cid=$(echo $row | awk '{ print $2 }')
-csec=$(echo $row | awk '{ print $3 }')
+akey=$(echo $row | cut -f1 -d "|"); cid=$(echo $row | cut -f2 -d "|")
+csec=$(echo $row | cut -f3 -d "|")
+
+echo $akey $cid $csec
 
 sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'$akey'<\/setting>/g' "$fname"
 sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'$cid'<\/setting>/g' "$fname"
