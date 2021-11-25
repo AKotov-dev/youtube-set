46854cab099c948d7fedb53deea32134142a0882
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-11-25 19:50:48 +0300

    the main script

--- youtube-set.sh
+++ youtube-set.sh
@@ -6,6 +6,11 @@
 
 fname="/home/$USER/.kodi/userdata/addon_data/plugin.video.youtube/settings.xml"
 
+if [[ ! -f "$fname" ]]; then
+    zenity --warning --no-wrap --text "The plugin.video.youtube not installed!"
+    exit 1;
+fi;
+
 row=$(zenity --forms --title="YouTube API Settings" --text="Input data" \
    --add-entry="API_Key" \
    --add-entry="Client_ID" \
@@ -19,5 +24,9 @@ csec=$(echo $row | cut -f3 -d "|")
 sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'$akey'<\/setting>/g' "$fname"
 sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'$cid'<\/setting>/g' "$fname"
 sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'$csec'<\/setting>/g' "$fname"
+
+zenity --info --no-wrap --text \
+"The API settings of the plugin.video.youtube were saved in the \
+file:\n/home/$USER/.kodi/userdata/addon_data/plugin.video.youtube/settings.xml"
 
 exit;
