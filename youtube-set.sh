94e515579bfa8c4e14874ef9a52b357bac59a202
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-11-25 20:50:15 +0300

    the main script

--- youtube-set.sh
+++ youtube-set.sh
@@ -18,19 +18,16 @@ row=$(zenity --forms --title="YouTube API Settings" --text="Input data" \
 
 [[ "$?" != "0" ]] && exit 1
 
-akey=$(echo $row | cut -f1 -d "|"); cid=$(echo $row | cut -f2 -d "|")
-csec=$(echo $row | cut -f3 -d "|")
+akey=$(echo $row | cut -f1 -d "|"); cid=$(echo $row | cut -f2 -d "|"); csec=$(echo $row | cut -f3 -d "|")
 
 if [[ "$akey" && "$cid" && "$csec" ]]; then
-sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'$akey'<\/setting>/g' "$fname"
-sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'$cid'<\/setting>/g' "$fname"
-sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'$csec'<\/setting>/g' "$fname"
+    sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'$akey'<\/setting>/g' "$fname"
+    sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'$cid'<\/setting>/g' "$fname"
+    sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'$csec'<\/setting>/g' "$fname"
 
-zenity --info --no-wrap --text \
-"The API settings of the plugin.video.youtube were saved in the \
-file:\n/home/$USER/.kodi/userdata/addon_data/plugin.video.youtube/settings.xml"
-    else
-zenity --warning --no-wrap --text "Empty values are not allowed!"
+    zenity --info --no-wrap --text "The API settings of the plugin.video.youtube were saved in the file:\n$fname"
+	else
+    zenity --warning --no-wrap --text "Empty values are not allowed!"
 fi;
 
 exit;
