89a33cf26a6fdad08fdc23e493495a007fe57530
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-11-27 08:25:39 +0300

    the main script (localization RU/EN)

--- youtube-set.sh
+++ youtube-set.sh
@@ -8,13 +8,13 @@
 case $LANG in
      ru_RU.UTF-8)
           str=('plugin.video.youtube не установлен!' \
-          'Настройки YouTube API для Kodi' \
+          'Настройки YouTube API' \
           'Настройки API plugin.video.youtube сохранены в файл:' \
           'Пустые значения недопустимы!')
           ;;
      *)
           str=('The plugin.video.youtube not installed!' \
-          'YouTube API Settings for Kodi' \
+          'YouTube API Settings' \
           'The API settings of the plugin.video.youtube saved in the file:' \
           'Empty values are not allowed!')
           ;;
@@ -30,7 +30,7 @@ if [[ ! -f "$fname" ]]; then
 fi;
 
 #Getting a string of parameters
-row=$(zenity --forms --title="" --text="${str[1]}" \
+row=$(zenity --forms --title="KODI" --text="${str[1]}" \
    --add-entry="API_Key" \
    --add-entry="Client_ID" \
    --add-entry="Client_Secret")
