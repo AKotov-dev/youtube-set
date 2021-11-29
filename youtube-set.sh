17006989f30d8dc0137d48b1dde3f91900debfe7
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-11-29 09:40:46 +0300

    the main script (localization RU/EN)

--- youtube-set.sh
+++ youtube-set.sh
@@ -7,14 +7,14 @@
 #Language select
 case $LANG in
      ru_RU.UTF-8)
-          str=('plugin.video.youtube не установлен!' \
-          'Настройки YouTube API' \
+          msg=('plugin.video.youtube не установлен!' \
+          'Настройка YouTube API' \
           'Настройки API plugin.video.youtube сохранены в файл:' \
           'Пустые значения недопустимы!')
           ;;
      *)
-          str=('The plugin.video.youtube not installed!' \
-          'YouTube API Settings' \
+          msg=('The plugin.video.youtube not installed!' \
+          'YouTube API Setting' \
           'The API settings of the plugin.video.youtube saved in the file:' \
           'Empty values are not allowed!')
           ;;
@@ -25,31 +25,28 @@ fname="/home/$USER/.kodi/userdata/addon_data/plugin.video.youtube/settings.xml"
 
 #The settings file exists?
 if [[ ! -f "$fname" ]]; then
-    zenity --warning --no-wrap --text "${str[0]}"
+    zenity --warning --no-wrap --text "${msg[0]}"
     exit 1;
 fi;
 
 #Getting a string of parameters
-row=$(zenity --forms --title="KODI" --text="${str[1]}" \
+row=($(zenity --forms --separator=" " --title="KODI" --text="${msg[1]}" \
    --add-entry="API_Key" \
    --add-entry="Client_ID" \
-   --add-entry="Client_Secret")
+   --add-entry="Client_Secret"))
 
 #Cancel or Close?
 [[ "$?" != "0" ]] && exit 1
 
-#Parsing the output into parameters
-akey=$(echo $row | cut -f1 -d "|"); cid=$(echo $row | cut -f2 -d "|"); csec=$(echo $row | cut -f3 -d "|")
-
 #If valid, insert the parameters into the settings file
-if [[ "$akey" && "$cid" && "$csec" ]]; then
-    sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'$akey'<\/setting>/g' "$fname"
-    sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'$cid'<\/setting>/g' "$fname"
-    sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'$csec'<\/setting>/g' "$fname"
+if [[ ${row[0]} && ${row[1]} && ${row[2]} ]]; then
+    sed -i 's/<setting id="youtube.api.key".*/<setting id="youtube.api.key">'${row[0]}'<\/setting>/g' "$fname"
+    sed -i 's/<setting id="youtube.api.id".*/<setting id="youtube.api.id">'${row[1]}'<\/setting>/g' "$fname"
+    sed -i 's/<setting id="youtube.api.secret".*/<setting id="youtube.api.secret">'${row[2]}'<\/setting>/g' "$fname"
 
-    zenity --info --no-wrap --text "${str[2]}\n$fname"
+    zenity --info --no-wrap --text "${msg[2]}\n$fname"
 	else
-    zenity --warning --no-wrap --text "${str[3]}"
+    zenity --warning --no-wrap --text "${msg[3]}"
 fi;
 
-exit;
+exit 0;
